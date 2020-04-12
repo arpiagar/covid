6f8f4b0d5143125c55bd27d51ab9a73a67342cbf
Arpit Agarwal <arpit.agarwal@booking.com>
2020-04-12 13:56:55 +0200

    Adding a conf.ini file to support API across different countries"

--- getApiData.py
+++ getApiData.py
@@ -1,13 +1,15 @@
 import requests
 import os
+import sys
 import plotly.graph_objects as go
 from collections import defaultdict
+from configparser import ConfigParser 
+CONFIG = ConfigParser() 
+CONFIG.read('conf.ini')
 proxies = {
   "http": None,
   "https": None,
 }
-APIFY_URL= "https://api.apify.com/v2/key-value-stores/lDegAca820XgvjE0C/records/LATEST?disableRedirect=true"
-HISTORICAL_DATA_URL= "https://api.apify.com/v2/datasets/jr5ogVGnyfMZJwpnB/items?format=json&clean=1"
 
 
 def fetch_url(url):
@@ -28,8 +30,8 @@ def generate_x_data(historic_data, x_axis_key):
 
 
 
-def plot_infected_deceased(y_axis_keys, x_axis_key):
-    historic_data = fetch_url(HISTORICAL_DATA_URL)
+def plot_infected_deceased(y_axis_keys, x_axis_key, url):
+    historic_data = fetch_url(url)
     fig = go.Figure()
     color = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)']
     x_data = [x[x_axis_key] for x in historic_data]
@@ -38,7 +40,7 @@ def plot_infected_deceased(y_axis_keys, x_axis_key):
         y_key = y_axis_keys[i]
         agg_map = defaultdict(int)
         for data in historic_data:
-            if data[y_key] is not None:
+            if y_key in data and data[y_key] is not None:
                 x_val = data[x_axis_key]
                 agg_map[x_val.split("T")[0]] += int(data[y_key])
         fig.add_trace(go.Scatter(x=list(agg_map.keys()),y=list(agg_map.values()),name=y_key, line=dict(color=color[i], width=4)))
@@ -50,6 +52,14 @@ def plot_infected_deceased(y_axis_keys, x_axis_key):
 
     fig.show()
 
-if __name__ == "__main__":
-    plot_infected_deceased(['deceased','infected'],'lastUpdatedAtApify')
+def parse_config_and_plot(country_code='NL'):
+    url = CONFIG.get(country_code,'HISTORICAL_DATA_URL')
+    y_axis_keys =  CONFIG.get(country_code,'API_FIELDS').split(",")
+    plot_infected_deceased(y_axis_keys, 'lastUpdatedAtApify', url)
 
+
+if __name__ == "__main__":
+    if sys.argv:
+        parse_config_and_plot(sys.argv[1])
+    else:
+        parse_config_and_plot()
